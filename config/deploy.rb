005b4505670d638d217744f8d1bca7cfde823d79
bengarlock <ben.garlock@gmail.com>
2022-07-17 14:56:20 -0400

    adding db creds

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,15 @@
 # config valid for current version and patch releases of Capistrano
 lock "~> 3.17.0"
 
-set :application, "my_app_name"
-set :repo_url, "git@example.com:me/my_repo.git"
+set :application, "portfolio_database_rails"
+set :repo_url, "https://github.com/bengarlock/portfolio_database_rails"
+
+# Deploy to the user's home directory
+set :deploy_to, "/home/deploy/#{fetch :application}"
+
+append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', '.bundle', 'public/system', 'public/uploads'
+
+set :keep_releases, 5
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
