60de088d1d67807112b43106c4ac59c73313a3fa
bengarlock <ben.garlock@gmail.com>
2022-07-17 16:38:46 -0400

    capistrano debugging

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,15 +5,15 @@ server '192.168.86.12', port: 22, roles: [:web, :app, :db], primary: true
 set :application, "portfolio_database_rails"
 set :repo_url, "https://github.com/bengarlock/portfolio_database_rails"
 
+# Deploy to the user's home directory
 set :user, 'deploy'
 set :puma_threads,    [4, 16]
 set :puma_workers,    0
-
+set :deploy_to, "/home/deploy/#{fetch :application}"
 set :pty,             true
 set :use_sudo,        false
 set :stage,           :production
 set :deploy_via,      :remote_cache
-set :deploy_to,       "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
 set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
 set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
 set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
@@ -25,8 +25,8 @@ set :puma_worker_timeout, nil
 set :puma_init_active_record, true  # Change to false when not using ActiveRecord
 
 
-# Deploy to the user's home directory
-set :deploy_to, "/home/deploy/#{fetch :application}"
+
+
 
 append :linked_files, "config/master.key"
 append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', '.bundle', 'public/system', 'public/uploads'
